c50d66c69895ca12cf9dfaae0843d73bd79ac3a8
e <2053793826@qq.com>
2020-04-09 14:48:53 +0800

    工具类

---
+++ src/main/java/com/niushunqian/utils/StreamUtil.java
@@ -0,0 +1,40 @@
+package com.niushunqian.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class StreamUtil {
+
+	/*
+	* 方法1：批量关闭流，参数能传入无限个。(10分)
+	* 例如传入FileInputStream对象、JDBC中Connection对象都可以关闭，并且参数个数不限。
+	*/
+	public static void closeAll(InputStream input) throws IOException{
+	//TODO 实现代码
+		input.close();
+	}
+	/*
+	* 方法2：传入一个文本文件对象，默认为UTF-8编码，返回该文件内容(10分)，要求方法内部调用上面第1个方法关闭流(5分)
+	*/
+	public static String readTextFile(InputStream src){
+	//TODO 实现代码
+		InputStreamReader inputStreamReader = new InputStreamReader(src);
+		
+		return "";
+	}
+	/*
+	* 方法3：传入文本文件对象，返回该文件内容(10分)，并且要求内部调用上面第2个方法(5分)。* 这是典型的方法重载，记住了吗？少年…
+	*/
+	public static String readTextFile(File txtFile) throws FileNotFoundException{
+	
+		return readTextFile(new FileInputStream(txtFile));
+			
+			
+	}
+
+
+}
